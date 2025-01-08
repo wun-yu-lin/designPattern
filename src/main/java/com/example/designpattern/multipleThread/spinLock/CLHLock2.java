27d694d53187da9acb3670e144ad5dd16f096c30
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2025-01-08 23:38:29 +0800

    CLHLock2 使用 atomicReference 實作

---
+++ src/main/java/com/example/designpattern/multipleThread/spinLock/CLHLock2.java
@@ -0,0 +1,79 @@
+package com.example.designpattern.multipleThread.spinLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicReference;
+//使用 AtomicReference 的版本
+public class CLHLock2 {
+    static Logger logger = LoggerFactory.getLogger("CLHLock2");
+
+    private AtomicReference<CLHNode> tail = new AtomicReference<>();
+
+    public static class CLHNode {
+        private volatile boolean isLocked = true;
+    }
+
+    public CLHNode getNode() {
+        return new CLHNode();
+    }
+
+    public void lock(CLHNode currThreadNode) {
+        CLHNode preNode = null;
+        while (true) {
+            //多個線程競爭寫入 FIFO queue
+            preNode = tail.get();
+            if (tail.compareAndSet(preNode, currThreadNode)) {
+                break;
+            }
+        }
+        if (preNode != null) {
+            while (preNode.isLocked) {
+                //當前驅node release lock, 下個線程解除自旋
+                //下個node 解除自旋後，就獲得鎖，接續工作
+            }
+        }
+        preNode = null;
+    }
+
+    public void unLock(CLHNode currThreadNode) {
+        //如果當前node不是 tail, CAS 操作會失敗, 而放掉當前的鎖
+        if (!tail.compareAndSet(currThreadNode, null)) {
+            currThreadNode.isLocked = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        CLHLock2 lock2 = new CLHLock2();
+
+        Runnable task = () -> {
+            CLHNode node = lock2.getNode();
+            try {
+                logger.info(Thread.currentThread().getName() + " is trying to acquire the lock...");
+                lock2.lock(node);
+                logger.info(Thread.currentThread().getName() + " has acquired the lock!");
+
+                // 模擬臨界區
+                Thread.sleep(1000);
+
+                logger.info(Thread.currentThread().getName() + " is releasing the lock...");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                lock2.unLock(node);
+            }
+
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+        Thread t3 = new Thread(task, "Thread-3");
+        Thread t4 = new Thread(task, "Thread-4");
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+
+}
